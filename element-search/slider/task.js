ca611d834db00b81bdd60e9ab5eed167af36ae3e
nika.movchanskaya <nika.movchanskaya@gmail.com>
2023-02-25 19:40:38 +0100

    slider updated

--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -1,64 +1,57 @@
 let slides = Array.from(document.querySelectorAll(".slider__item"));
 let sliderDots = Array.from(document.querySelectorAll(".slider__dot"));
 
-function activateCurrent(currentActiveNumber) {
-    slides[currentActiveNumber].className += " slider__item_active";
-    sliderDots[currentActiveNumber].className += " slider__dot_active";
+function activateNew(num) {
+    slides[num].classList.add("slider__item_active");
+    sliderDots[num].classList.add("slider__dot_active");
 }
 
-function deactivatePrevious(previousNum) {
-    slides[previousNum].className = "slider__item";
-    sliderDots[previousNum].className = "slider__dot";
+function deactivateCurrent(num) {
+    slides[num].classList.remove("slider__item_active");
+    sliderDots[num].classList.remove("slider__dot_active");
+}
+
+function setNewImage(index) {
+    //find currently active slide
+    let currentActiveNumber = slides.findIndex((item) => item.classList.contains("slider__item_active"));
+    let newActiveNumber = index;
+
+    //deactivate it
+    deactivateCurrent(currentActiveNumber);
+
+    //validate new active slide number
+    if (newActiveNumber < 0) {
+        newActiveNumber = slides.length - 1;
+    }
+    else if (newActiveNumber > slides.length - 1) {
+        newActiveNumber = 0;
+    }
+
+    //activate it
+    activateNew(newActiveNumber);
 }
 
 document.querySelector(".slider__arrow_prev").onclick = function() {
 
-    let currentActiveNumber = slides.findIndex((item) => item.className.includes("slider__item_active"));
-    if (currentActiveNumber === 0) {
-        currentActiveNumber = slides.length - 1;
-
-        //deactivate previously selected photo and dot
-        deactivatePrevious(0);
-    }
-    else {  
-        currentActiveNumber--;   
-
-        //deactivate previously selected photo and dot
-        deactivatePrevious(currentActiveNumber + 1);
-    }
-
-    //activate new photo and dot
-    activateCurrent(currentActiveNumber);
+    let currentActiveNumber = slides.findIndex((item) => item.classList.contains("slider__item_active"));
+    
+    setNewImage(currentActiveNumber - 1);
 };
 
 document.querySelector(".slider__arrow_next").onclick = function() {
     
-    let currentActiveNumber = slides.findIndex((item) => item.className.includes("slider__item_active"));
-    if (currentActiveNumber === slides.length - 1) {
-        currentActiveNumber = 0;
-
-        //deactivate previously selected photo and dot
-        deactivatePrevious(slides.length - 1);
-    }
-    else {
-        currentActiveNumber++;
-
-        //deactivate previously selected photo and dot
-        deactivatePrevious(currentActiveNumber - 1);
-    }
-
-    //activate new photo and dot
-    activateCurrent(currentActiveNumber);
+    let currentActiveNumber = slides.findIndex((item) => item.classList.contains("slider__item_active"));
+    
+    setNewImage(currentActiveNumber + 1);
 };
 
 sliderDots.forEach((item, index) => {
     item.onclick = function() {
-        let currentActiveNumber = slides.findIndex((item) => item.className.includes("slider__item_active"));
+        let currentActiveNumber = slides.findIndex((item) => item.classList.contains("slider__item_active"));
         
-        if (index !== currentActiveNumber) {       
-            deactivatePrevious(currentActiveNumber);
-            currentActiveNumber = index;
-            activateCurrent(currentActiveNumber);
+        if (index !== currentActiveNumber) {  
+            
+            setNewImage(index);
         }
     }
 });
